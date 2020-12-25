19015542aef52cb9adae440273e7b95c3f3bed02
mTarik12 <72448312+mTarik12@users.noreply.github.com>
2020-12-25 21:25:16 +0200

    first steps

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Drakula',
+          id: 'asr2'
+        },
+        {
+          name: 'Zombie',
+          id: 'as1w'
+        },
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
